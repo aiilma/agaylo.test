5b7b591566950a6f864c29fda83b5a640328db59
patilapa <vitaly.mortikov@mail.ru>
2020-03-28 17:35:37 +0300

    some part of migrations && manager's creds generation with seeding

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // roles
+        DB::table('roles')->insert([
+            ['name' => 'client'],
+            ['name' => 'manager'],
+        ]);
+
+        // manager
+        $user = User::create([
+            'name' => 'agaylo manager',
+            'email' => env('MANAGER_EMAIL', 'manager@agaylo.test'),
+            'password' => Hash::make(env('MANAGER_PASSWORD', 'dvorak')),
+        ]);
+
+        // role binding
+        DB::table('user_roles')->insert([
+            'user_id' => $user->id,
+            'role_id' => 2,
+        ]);
+    }
+}
