e1d007663177b045a625d66ffab3db2ad2be744c
patilapa <vitaly.mortikov@mail.ru>
2020-03-30 00:02:40 +0300

    client: view list of requests is done

--- resources/views/ui/filter.blade.php
+++ resources/views/ui/filter.blade.php
@@ -2,24 +2,24 @@
     <div class="row py-1">
         <div class="col-4">
             <div class="btn-group special" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-success">Просмотренные</button>
-                <button type="button" class="btn btn-danger">Непросмотренные</button>
+                <a class="btn btn-success" href="#" role="button">Просмотренные</a>
+                <a class="btn btn-danger" href="#" role="button">Непросмотренные</a>
             </div>
         </div>
     </div>
     <div class="row py-1">
         <div class="col-4">
             <div class="btn-group special" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-success">Открытые</button>
-                <button type="button" class="btn btn-danger">Закрытые</button>
+                <a class="btn btn-success" href="#" role="button">Открытые</a>
+                <a class="btn btn-danger" href="#" role="button">Закрытые</a>
             </div>
         </div>
     </div>
     <div class="row py-1">
         <div class="col-4">
             <div class="btn-group special" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-success">С ответом</button>
-                <button type="button" class="btn btn-danger">Без ответа</button>
+                <a class="btn btn-success" href="#" role="button">С ответом</a>
+                <a class="btn btn-danger" href="#" role="button">Без ответа</a>
             </div>
         </div>
     </div>
